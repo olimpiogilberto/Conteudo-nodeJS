8302408fd3bafce9578a71c6079f78dd8b3dd924
c129427 <olimpiogilberto@gmail.com>
2019-07-06 09:39:56 -0300

    nao lembro
    teste giba

--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -5,6 +5,55 @@ module.exports = function(app){
         res.send('OK.');
     });
 
+    app.put('/pagamentos/pagamento/:id', function(req, res){
+
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = 'CONFIRMADO';
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.atualiza(pagamento, function(erro){
+
+            if (erro){
+                res.status(500).send(erro);
+                return;
+            }
+            res.send(pagamento);
+
+        });
+
+
+    });
+    
+    app.delete('/pagamentos/pagamento/:id', function(req, res){
+
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = 'CANCELADO';
+
+        var connection = app.persistencia.connectionFactory();
+        var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+        pagamentoDao.atualiza(pagamento, function(erro){
+
+            if (erro){
+                res.status(500).send(erro);
+                return;
+            }
+            console.log('pagamento cancelado');
+            res.status(204).send(pagamento);
+
+        });
+
+
+    });
+    
     app.post('/pagamentos/pagamento', function(req, res){
 
         req.assert("forma_de_pagamento",
